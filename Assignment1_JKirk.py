0dc0b51c813cc0ed524ab806acebaa7cd7e4c5f3
Jamey Kirk <jameykirk@gmail.com>
2021-03-04 21:15:04 +0900

    Intial Commit

---
+++ Assignment1_JKirk.py
@@ -0,0 +1,7 @@
+"""
+Author: Jamey Kirk
+Date: 03/04/2021
+Assignment: Find number of days in a month
+Description: Program takes a given month and year and returns the number of days in that month.
+"""
+
